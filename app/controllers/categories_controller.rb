aa10e1af988755a48e02014073e5820766f0d469
Evelyn Navarrete <evelyn.navarrete@outlook.com>
2015-04-14 17:03:06 +0000

    etsy app

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,33 @@
+class CategoriesController < ApplicationController
+  def index
+    @categories = Category.all 
+  end
+
+  def show
+    @category = Category.find(params[:id])
+  end
+
+  def new
+    @category = Category.new 
+  end
+
+  def create 
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to(:action => 'index')
+    else
+      render('new')
+    end 
+  end 
+  
+  def edit
+  end
+
+  def delete
+  end
+  
+  private
+  def category_params 
+    params.require(:category).permit(:name, :thumbrl)
+end
+end 
